064aeebf0c3ec7ff48866d0f96d88e8c2d9efe16
tomis007 <thomas.hendrickson@tufts.edu>
2016-05-07 20:22:26 -0400

    Updated to read in gameboy ROM file

--- src/gameboi/GBMem.java
+++ src/gameboi/GBMem.java
@@ -5,20 +5,39 @@
  */
 package gameboi;
 
+
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.IOException;
+
 /**
  *
  * @author thomas
  */
 public class GBMem {
     private int memory[];
-    private byte cartridge[];
+    private int cartridge[];
     
-    
-    
-    public GBMem() {
-        memory = new int[0x200000];
-        cartridge = new byte[0x400];
+        
+    public GBMem(Path path) {
+        memory = new int[0x10000];
+        
+        try {
+            byte[] rom = Files.readAllBytes(path);
+            cartridge = new int[rom.length];
+            
+            for (int i = 0; i < rom.length; ++i) {
+                cartridge[i] = Byte.toUnsignedInt(rom[i]);
+                System.out.println(cartridge[i]);
+            }
+            System.out.println("Number of bytes in file: " + rom.length);
+
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
     }
+
     
     public int readByte(int address) {
         return 0;
