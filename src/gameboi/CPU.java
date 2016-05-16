1382dc8f80b90d190721287d5c9078fe77415703
tomis007 <thomas.hendrickson@tufts.edu>
2016-05-15 23:28:30 -0400

    added basic state representations

--- src/gameboi/CPU.java
+++ src/gameboi/CPU.java
@@ -5,10 +5,60 @@
  */
 package gameboi;
 
+
+
+
 /**
- *
+ * Z80 Gameboy CPU
+ * 
+ * Implementation of a gameboy cpu. Eight registers (a,b,c,d,e,f,h,l), a
+ * stack pointer, program counter
+ * 
  * @author tomis007
  */
 public class CPU {
     
+    // registers
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+    private int f;
+    private int h;
+    private int l;
+    
+    // stack pointer
+    private int sp;
+    // program counter
+    private int pc;
+    
+    
+    private GBMem memory;
+    
+    
+    public CPU(GBMem memory) {
+        this.memory = memory;
+    }
+    
+    /**
+     * Execute the next opcode in memory
+     * 
+     * @param memory GBMem memory to access
+     * @return clock cycles taken to execute the opcode
+     */ 
+    public int ExecuteOpcode(GBMem memory) {
+        
+        int opcode = memory.readByte(pc);
+        pc++;
+        return runInstruction(opcode);
+    }
+    
+    
+    private int runInstruction(int opcode) {
+        
+      return 0;  
+    };
+    
 }
+
