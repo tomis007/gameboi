9515464195d08f9c7c2242c80cb34e35b2d92cc1
tomis007 <thomas.hendrickson@tufts.edu>
2016-07-05 23:07:40 +0200

    added rudimentary cpu timing

--- src/gameboi/GameBoi.java
+++ src/gameboi/GameBoi.java
@@ -15,27 +15,42 @@ public class GameBoi {
      * @param argv the command line arguments
      */
     public static void main(String[] argv) {
-        
+
         Path rom_path = Paths.get(argv[0]);
-        
+
         GBMem memory = new GBMem(rom_path);
-        
+
         CPU z80 = new CPU(memory);
 
         GPU gpu = new GPU(memory, z80);
-        
+
         z80.setGPU(gpu);
-        int count = 0;
         while (true) {
-            int cycles;
-//            count++;
-            cycles = z80.ExecuteOpcode();
-            gpu.updateGraphics(cycles);
+            int count = 69905;
+//            long startTime = System.nanoTime();
+            while (count > 0) {
+                int cycles;
+                cycles = z80.ExecuteOpcode();
+                gpu.updateGraphics(cycles);
+                count -= cycles;
+            }
+//            long endTime = System.nanoTime();
+//            if (endTime - startTime < (1000000000 / 60)) {
+//                System.out.println("Should sleep: " + (endTime - startTime));
+//                try {
+//                    System.out.println("sleeping");
+//                    Thread.sleep((endTime - startTime) / 1000000);
+//                } catch (InterruptedException e) {
+//                    System.out.println("ugh");
+//                }
+//            }
+
+
 //            System.out.println(count);
         }
-  //      System.out.println(count);
+        //      System.out.println(count);
 //        System.exit(1);
 
     }
-    
 }
+//}
