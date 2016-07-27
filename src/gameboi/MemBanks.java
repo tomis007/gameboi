f19e12216c3f9911164f8d8c7af15226f5be1710
tomis007 <thomas.hendrickson@tufts.edu>
2016-07-27 14:57:02 +0200

    added MBC1

--- src/gameboi/MemBanks.java
+++ src/gameboi/MemBanks.java
@@ -32,6 +32,13 @@ public class MemBanks {
             case 0:
                 memBank = new MBC0(romCartridge);
                 break;
+            case 1:
+                memBank = new MBC1(romCartridge);
+                break;
+            case 2:
+                memBank = new MBC1(romCartridge);
+            case 3:
+                memBank = new MBC1(romCartridge);
             default:
                 System.err.println("Sorry, this MBC is not implemented yet");
                 System.exit(1);
@@ -41,85 +48,26 @@ public class MemBanks {
     /**
      * read a Byte from the RomBank
      * 
-     * <p> The address to be read must be between (inclusive) 0x4000 and 
-     *     0x7FFF. This is reading from the switchable ROM Bank in the
-     *     gameboy's memory.
-     * 
+     * <p> The address to be read must be between located in ROM 0 -
+     *     0x7FFF, or an external Ram address
+     *
      * @param address (required) address of byte to read
-     * @return int that is the 'byte' at the address. If invalid address, 
-     *     -1 is returned
-     */ 
+     * @return int that is the 'byte' at the address
+     */
     public int readByte(int address) {
         return memBank.readByte(address);
     }
-    
-    /**
-     * Returns whether or not ram bank writing is enabled
-     *
-     * TODO
-     * @return true if ram bank writing enabled, false if not
-     */ 
-    public boolean isRamEnabled() {
-        return true;
-    }
-
 
     /**
+     * write a byte to external ram or to
+     * a MBC controller
      *
      *
-     *
-     * @param address
-     * @param data
+     * @param address to write to (ext RAM or ROM)
+     * @param data to write
      */
     public void writeByte(int address, int data) {
         memBank.writeByte(address, data);
     }
 
-    /**
-     * updates the Memory Banks
-     * 
-     * <p> Bank updates are triggered when the gameboy attempts to write
-     *     to the ROM addresses (0x0-0x7fff) in the gameboy memory.
-     * 
-     * @param address int address the write to
-     * @param data int data to write to
-     */ 
-    public void updateBanking(int address, int data) {
-/*        if (address < 0x2000) {
-            if (bankMode == Mode.MBC1) {
-                int mode = data & 0xf;
-                if (mode == 0xa) {
-                    ramEnabled = true;
-                }
-                if (mode == 0x0) {
-                    ramEnabled = false;
-                }       
-            }
-        } else if ((address >= 0x2000) && (address < 0x4000)) {
-            if (bankMode == Mode.MBC1) {
-                curROMBank &= 0xe0;
-                curROMBank |= (data & 0x1f);
-                curROMBank += (curROMBank == 0) ? 1 : 0;
-            } //nelse if (bankMode == Mode.MBC2) {
-              //  curROMBank = data & 0xf;
-//            }
-        } else if ((address >= 0x4000) && (address < 0x6000)) {
-            if (bankMode == Mode.MBC1) {
-                if (isRomBanking) {
-                    curROMBank &= 0x1f;
-                    curROMBank |= data & 0xe0;
-                } else {
-                    curRAMBank = data & 0x3;
-                }
-            }
-        } else if ((address >= 0x6000) && (address < 0x8000)) {
-            if (bankMode == Mode.MBC1) {
-                int nextMode = 0x1 & data;
-                isRomBanking = (nextMode == 0);
-                if (isRomBanking) {
-                    curRAMBank = 0;
-                }
-            }
-        }*/
-    }
 }
