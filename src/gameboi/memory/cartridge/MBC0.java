acd9ab3aa999c804043f14036b54455bfb0edaf6
tomis007 <thomas.hendrickson@tufts.edu>
2016-08-05 10:18:10 +0200

    added license

--- src/gameboi/memory/cartridge/MBC0.java
+++ src/gameboi/memory/cartridge/MBC0.java
@@ -1,4 +1,27 @@
-package gameboi;
+/*
+ * The MIT License
+ *
+ * Copyright 2016 tomis007.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package gameboi.memory.cartridge;
 
 /**
  *
@@ -72,6 +95,7 @@ public class MBC0 implements MemoryBank {
      * @param data to write
      */
     public void writeByte(int address, int data) {
+        data &= 0xff;
         if (address >= 0xa000 && address < 0xc000) {
             extRam[address - 0xa000] = data & 0xff;
         }
