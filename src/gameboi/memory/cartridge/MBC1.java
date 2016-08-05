f6d18ca86ff16224eb750d3b7b5081557005129a
tomis007 <thomas.hendrickson@tufts.edu>
2016-08-05 10:18:45 +0200

    added license, moved

--- src/gameboi/memory/cartridge/MBC1.java
+++ src/gameboi/memory/cartridge/MBC1.java
@@ -1,10 +1,30 @@
-package gameboi;
+/*
+ * The MIT License
+ *
+ * Copyright 2016 tomis007.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */package gameboi.memory.cartridge;
 
 /**
  *
- * MBC1 cartridge chip for membanks
- *
- *
+ * Implementation of MBC1 cartridge chip
  *
  * tomis007
  */
@@ -55,12 +75,10 @@ public class MBC1 implements MemoryBank {
     }
 
     /**
-     *
-     *
-     *
+     * Read a byte from MBC1
      *
      * @param address to read from
-     * @return
+     * @return byte at address
      */
     public int readByte(int address) {
         //bank 0 always here
@@ -87,7 +105,7 @@ public class MBC1 implements MemoryBank {
 
     /**
      * controls the MBC1 registers
-     *
+     * or writes to RAM BANK
      *
      * @param address to write to
      * @param data to write
